01f7381fac3bb1ef59d0ae61c87d5d6b418ac63f
Sonia Gu <ssgu321@gmail.com>
2018-05-22 10:14:12 -0700

    database

--- app/src/main/java/com/example/gus1430/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/gus1430/mycontactapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.gus1430.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+        Log.d("myContactApp", "MainActivity: Instantiated myDb");
+
     }
 }
