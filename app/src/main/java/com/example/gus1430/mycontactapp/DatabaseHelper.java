d19946d37956673ea7bdf949c1c75b469f8b3ff2
Sonia Gu <ssgu321@gmail.com>
2018-05-25 10:48:58 -0700

    Contact List

--- app/src/main/java/com/example/gus1430/mycontactapp/DatabaseHelper.java
+++ app/src/main/java/com/example/gus1430/mycontactapp/DatabaseHelper.java
@@ -2,13 +2,14 @@ package com.example.gus1430.mycontactapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "Contact2018.db";
     public static final String TABLE_NAME = "Contact2018_table";
     public static final String ID = "ID";
@@ -19,9 +20,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + TABLE_NAME + " (" +
                     ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    COLUMN_NAME_CONTACT + " TEXT" +
-                    COLUMN_ADDRESS_CONTACT + "TEXT," +
-                    COLUMN_PHONENUM_CONTACT + "TEXT)";
+                    COLUMN_NAME_CONTACT + " TEXT," +
+                    COLUMN_ADDRESS_CONTACT + " TEXT," +
+                    COLUMN_PHONENUM_CONTACT + " TEXT)";
 
     public static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + TABLE_NAME;
@@ -36,6 +37,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         Log.d("myContactApp", "DatabaseHelper: creating database");
         db.execSQL(SQL_CREATE_ENTRIES);
     }
+
+    /*
+    public void onOpen(SQLiteDatabase db) {
+        Log.d("myContactApp", "DatabaseHelper: opening database");
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }*/
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -68,6 +76,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             Log.d("myContactApp", "DatabaseHelper: Contact insert -- PASSED");
             return true;
         }
+    }
+
+    public Cursor getAllData(){
+        Log.d("myContactApp", "DatabaseHelper: Pulling all records from the database");
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
+        return res;
     }
 
 }
--- app/src/main/java/com/example/gus1430/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/gus1430/mycontactapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gus1430.mycontactapp;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         editName = findViewById(R.id.editText_name);
+        editAddress = findViewById(R.id.editText_address);
+        editNum = findViewById(R.id.editText_num);
         myDb = new DatabaseHelper(this);
         Log.d("myContactApp", "MainActivity: Instantiated myDb");
     }
@@ -28,16 +32,53 @@ public class MainActivity extends AppCompatActivity {
     public void addData(View view){
         Log.d("myContactApp", "MainActivity: Add contact button pressed");
 
-        boolean isInsertedName = myDb.insertData(editName.getText().toString(), null, null);
-        boolean isInsertedAddress = myDb.insertData(editName.getText().toString(), null, null);
-        boolean isInsertedNum = myDb.insertData(editName.getText().toString(), null, null);
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAddress.getText().toString(), editNum.getText().toString());
 
-        if(isInsertedName == true){
+        if(isInserted == true){
             Toast.makeText(MainActivity.this, "Success! Contact Inserted", Toast.LENGTH_LONG).show();
         }
         else {
-            Toast.makeText(MainActivity.this, "Failed -- Contact Inserted", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Failed -- Contact Not Inserted", Toast.LENGTH_LONG).show();
         }
 
     }
+
+    public void viewData(View view){
+        Cursor res = myDb.getAllData();
+
+        Log.d("myContactApp", "MainActivity: viewData: received cursor");
+
+        if(res.getCount() == 0){
+            showMessage("Error", "No data found in the database");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            //append the res col 0, 1, 2, 3 -- see the StringBuffer and Cursor API
+            //Delimit each of the "appends" with a line feed "\n"
+            for(int i = 0; i < 4; i++){
+                buffer.append(res.getString(i));
+                buffer.append("\n");
+            }
+            buffer.append("\n");
+
+        }
+
+        Log.d("myContactApp", "MainActivity:StringBuffer Created");
+
+        showMessage("Data", buffer.toString());
+    }
+
+    private void showMessage(String title, String message) {
+        Log.d("myContactApp", "MainActivity: Assembling alert dialogue");
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+
+    }
+
+
 }
